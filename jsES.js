be408844c150fef4b265914b46634c1921573147
Omer <omerh127@gmail.com>
2018-07-17 23:30:20 +0300

    succeeded interacting with mouseover

--- jsES.js
+++ jsES.js
@@ -4,7 +4,7 @@ function  building_grid(size) {
   for (let i = 0; i < size; i++){
     for (let j = 0; j < size; j++){
       const box = document.createElement('div');
-      box.classList.add('untouched');
+      box.classList.add("untouched");
       box.style.width = "calc((100% / " + size + " - 4px)";
       box.style.height = "calc((100% / " + size + " - 4px)"; // size that fit's to the container, substracting the borders
       container.appendChild(box);
@@ -12,4 +12,18 @@ function  building_grid(size) {
   }
 }
 
+//function turnBlack(index) {
+//  let pixel = document.getElementsByClassName("untouched");
+//  pixel[index].style.backgroundColor = "red";
+//}
+
 building_grid(16);
+
+var pixels = document.getElementsByClassName("untouched");
+
+for (let i = 0; i < pixels.length; i++){
+  pixels[i].addEventListener("mouseover", ()=>{
+    pixels[i].style.backgroundColor = "black";
+    pixels[i].style.border = "2px solid white";
+  });
+}
